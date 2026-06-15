e0841bf50c15a34f005a271bdd592211d9871280
Mohammad Ridwan <ridwannn.336@gmail.com>
2026-06-15 14:16:24 +0700

    chore: update cors origin for production frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,12 +11,10 @@ import routes from "./routes";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: ["https://pengaduan-opal.vercel.app", "http://localhost:3000"],
+  credentials: true
+}));
 
 app.use(
   helmet({
